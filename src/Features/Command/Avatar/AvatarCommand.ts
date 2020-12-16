39e09639dca65ea347484b3b77810c507389d67f
Antoine <gm.anqo@gmail.com>
2020-12-16 21:38:13 +0100

    feat(commands): add `-avatar <url>` command to change bot avatar

---
+++ src/Features/Command/Avatar/AvatarCommand.ts
@@ -0,0 +1,30 @@
+import { TYPES } from "@/App/AppTypes";
+import { Client, Message } from "discord.js";
+import { inject, injectable } from "inversify";
+import AbstractCommand from "../AbstractCommand";
+import fetch from "node-fetch";
+import Constant from "@/Domain/Constant";
+import { Response } from "puppeteer";
+import AvatarErrorHandler from "./AvatarErrorHandler";
+
+@injectable()
+export default class AvatarCommand extends AbstractCommand {
+  public keyword = "avatar";
+
+  constructor(
+    @inject(TYPES.CLIENT) private client: Client,
+    @inject(TYPES.MESSAGE_CONTENT_LOWERED) private content: string,
+    @inject(TYPES.AVATAR_ERROR_HANDLER) private errorHandler: AvatarErrorHandler,
+  ) {
+    super();
+  }
+
+  public async execute() {
+    try {
+      const URL = this.content.split(" ")[1];
+      await this.client.user?.setAvatar(URL);
+    } catch (error) {
+      await this.errorHandler.handle(error);
+    }
+  }
+}
