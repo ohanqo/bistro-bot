ca79860a689ae78184d09bf94f60b45a2a904ae7
Antoine <gm.anqo@gmail.com>
2020-12-27 02:56:09 +0100

    chore(database): use url to connect

--- src/Features/_shared/DatabaseModule.ts
+++ src/Features/_shared/DatabaseModule.ts
@@ -5,7 +5,16 @@ import WebsiteWatcherEntity from "../Command/WebsiteWatcher/WebsiteWatcherEntity
 
 const databaseModule = new AsyncContainerModule(async (bind: interfaces.Bind) => {
   const manager = getConnectionManager();
-  const connection = manager.has("default") ? getConnection() : await createConnection();
+  const connection = manager.has("default")
+    ? getConnection()
+    : await createConnection({
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        synchronize: false,
+        entities: ["dist/**/*Entity.js"],
+        migrations: ["dist/App/Migration/*.js"],
+        migrationsRun: true,
+      });
   const websiteWatcherRepository = connection.getRepository(WebsiteWatcherEntity);
 
   bind(TYPES.CONNECTION).toConstantValue(connection);
